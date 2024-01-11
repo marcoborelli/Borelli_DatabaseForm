f5422591319593134c2bdd5aa0f37a62d4124e33
marcoborelli <marco05.borelli@gmail.com>
2024-01-11 21:57:27 +0100

    Form1: aggiunto metodo get che in automatico restituisce un oggetto MySqlConnection con la connessione aperta

--- Borelli_DatabaseForm/Form1.cs
+++ Borelli_DatabaseForm/Form1.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using MySql.Data.MySqlClient;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Borelli_DatabaseForm {
     public partial class Form1 : Form {
+        private MySqlConnection _sqlConn;
+
+        private MySqlConnection SqlConnection {
+            get {
+                if (_sqlConn == null) {
+                    try {
+                        _sqlConn = new MySqlConnection("server=84.33.120.138; port=3306; uid=programma; pwd=12345; database=AziendaImpiegatiProgetti");
+                    } catch (Exception e) {
+                        MessageBox.Show(e.Message);
+                    }
+                }
+
+                _sqlConn.Open(); //non so se sia logicamente giusto aprirla di default
+
+                int attemps = 0;
+                bool first = true;
+                do {
+                    attemps++;
+
+                    if (first) {
+                        first = false;
+                    } else {
+                        Thread.Sleep(250);
+                    }
+                } while (!_sqlConn.Ping() && attemps < 15);
+
+                if (attemps == 15) {
+                    throw new Exception("Il server non risponde al ping");
+                }
+
+                return _sqlConn;
+            }
+        }
         public Form1() {
             InitializeComponent();
         }
